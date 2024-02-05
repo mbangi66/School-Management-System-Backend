e24b3796e73acc8e35aa2262b4bc9f1f1084f34a
Mustaqeem <mbangi66@gmail.com>
2024-02-05 11:04:24 +0300

    Endpoint for filtering students

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('./db');
+const {getFilteredStudents , db} = require('./db');
 const bodyParser = require('body-parser');
 const userRoutes = require('./userRoutes');
 const cors = require('cors');
@@ -15,7 +15,7 @@ app.use('/users', userRoutes);
 app.use(bodyParser.json());
 
 // Define a route to fetch students from the database
-app.get('/students', (req, res) => {
+app.get('/students', async (req, res) => {
   db.all('SELECT * FROM students', (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -26,17 +26,17 @@ app.get('/students', (req, res) => {
 });
 
 // Add a new student to the database
-app.post('/students', (req, res) => {
-  const { name, grade, age, subject, year, type, photo } = req.body;
+app.post('/students', async (req, res) => {
+  const { name, grade, age, subject, year, classNumber, type, photo } = req.body;
 
-  if (!name || !grade || !age || !subject || !year || !type || !photo) {
+  if (!name || !grade || !age || !subject || !year || !classNumber || !type || !photo) {
     res.status(400).json({ error: 'All fields are required.' });
     return;
   }
 
   db.run(
-    'INSERT INTO students (name, grade, age, subject, year, type, photo) VALUES (?, ?, ?, ?, ?, ?, ?)',
-    [name, grade, age, subject, year, type, photo],
+    'INSERT INTO students (name, grade, age, subject, year, classNumber, type, photo) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+    [name, grade, age, subject, year, classNumber, type, photo || null],
     function (err) {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -48,14 +48,16 @@ app.post('/students', (req, res) => {
   );
 });
 
+
+
 // Update an existing student
-app.put('/students/:id', (req, res) => {
-  const { name, grade, age, subject, year, type, photo } = req.body;
+app.put('/students/:id', async (req, res) => {
+  const { name, grade, age, subject, year, classNumber, type, photo } = req.body;
   const studentId = req.params.id;
 
   db.run(
-    'UPDATE students SET name=?, grade=?, age=?, subject=?, year=?, type=?, photo=? WHERE id=?',
-    [name, grade, age, subject, year, type, photo, studentId],
+    'UPDATE students SET name=?, grade=?, age=?, subject=?, year=?, classNumber=?, type=?, photo=? WHERE id=?',
+    [name, grade, age, subject, year, classNumber, type, photo || null, studentId],
     function (err) {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -67,8 +69,9 @@ app.put('/students/:id', (req, res) => {
   );
 });
 
+
 // Delete a student
-app.delete('/students/:id', (req, res) => {
+app.delete('/students/:id', async (req, res) => {
   const studentId = req.params.id;
 
   db.run('DELETE FROM students WHERE id=?', [studentId], function (err) {
@@ -79,6 +82,19 @@ app.delete('/students/:id', (req, res) => {
 
     res.json({ success: true });
   });
+});
+
+// Endpoint for filtering students
+app.get('/filtered-students', async (req, res) => {
+  const { filter, year, classNumber } = req.query;
+
+  try {
+    const filteredStudents = await getFilteredStudents(filter, year, classNumber);
+    res.json({ students: filteredStudents });
+  } catch (error) {
+    console.error('Error fetching filtered students', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.get('/protected', authenticateToken, (req, res) => {
